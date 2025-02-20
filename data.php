ff319ab0b3055fd9f9d7c7b8282706b04a4a96bd
kemasmyunus <7a9tqplxjh@gmail.com>
2025-02-20 20:52:28 +0800

    update: belajar menampilkan data menggunakan pdo

--- data.php
+++ data.php
@@ -1,19 +1,12 @@
 <?php
+include "helper.php";
 
-require_once('helper.php');
+$connection = getConnection();
+$sql = "SELECT * FROM list";
+$result = $connection->query($sql);
+foreach($result as $row){
+    $id = $row['id'];
+    $todo = $row['todo'];
 
-$query = "SELECT * FROM list";
-$sql = mysqli_query($db_connect, $query);
-
-if($sql){
-    $result = array();
-    while($row = mysqli_fetch_array($sql)){
-        array_push($result, array(
-            'id' => $row['id'],
-            'todo' => $row['todo']
-        ));
-    }
-
-    echo json_encode(array('todo' => $result)); 
+    echo "$id. $todo".PHP_EOL;
 }
-?>
