21b7148055bb7b2775b494eac780c6c62670bf41
kemasmyunus <7a9tqplxjh@gmail.com>
2025-02-20 21:02:56 +0800

    update: belajar menambahkan data ke sql menggunakan pdo

--- create.php
+++ create.php
@@ -1,15 +1,9 @@
 <?php
 
-require_once('helper.php');
-
-$todo = $_POST['todo'];
-$query = "INSERT INTO list(todo) VALUES ('$todo')";
-$sql = mysqli_query($db_connect, $query);
-
-if($sql){
-    echo json_encode(array('message' => 'created!')); 
-}else{
-    echo json_encode(array('message' => 'error!')); 
-}
-
+include "helper.php";
+$connection = getConnection();
+// $todo = $_POST['todo'];
+$query = "INSERT INTO list(todo) VALUES ('Kouka')";
+$connection->exec($query);
+$connection = null;
 ?>
--- data.php
+++ data.php
@@ -10,3 +10,5 @@ foreach($result as $row){
 
     echo "$id. $todo".PHP_EOL;
 }
+
+$connection = null;
