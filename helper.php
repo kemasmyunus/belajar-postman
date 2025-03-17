8237aaf517417815b57cae3f1f1ccec23b3c6cc6
kemasmyunus <7a9tqplxjh@gmail.com>
2025-03-17 20:13:27 +0800

    update: memperbarui api

--- helper.php
+++ helper.php
@@ -1,16 +1,20 @@
 <?php
 
 function getConnection(){
-    
     try{
         $host = "localhost";
         $port = 3306;
         $database = "db_catatanlist";
         $username = "root";
         $password = "";
-        return new PDO("mysql:host=$host:$port;dbname=$database",$username,$password);
+        return new PDO("mysql:host=$host;port=$port;dbname=$database", $username, $password);
     }catch(PDOException $exception){
-        echo "WADUH ERROR, NIH ERRORNYA : ".$exception->getMessage().PHP_EOL;
+        handleError($exception->getMessage());
     }
+}
+
+function handleError($message) {
+    echo "WADUH ERROR, NIH ERRORNYA : " . $message . PHP_EOL;
+    exit;
 }
 
