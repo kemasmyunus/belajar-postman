9ca0d949820779ba2c060c1601ac83fa43bf1dbe
kemasmyunus <7a9tqplxjh@gmail.com>
2025-03-17 20:13:13 +0800

    update: memperbarui api

--- create.php
+++ create.php
@@ -1,9 +1,26 @@
 <?php
 
 include "helper.php";
+
 $connection = getConnection();
-// $todo = $_POST['todo'];
-$query = "INSERT INTO list(todo) VALUES ('Kouka')";
-$connection->exec($query);
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (isset($_POST['todo'])) {
+        $todo = $_POST['todo'];
+        $query = "INSERT INTO list(todo) VALUES (:todo)";
+        $statement = $connection->prepare($query);
+        $statement->bindParam(':todo', $todo);
+        if ($statement->execute()) {
+            echo "Data successfully inserted.";
+        } else {
+            handleError("Failed to insert data.");
+        }
+    } else {
+        handleError("No 'todo' field in POST data.");
+    }
+} else {
+    handleError("Invalid request method.");
+}
+
 $connection = null;
 ?>
